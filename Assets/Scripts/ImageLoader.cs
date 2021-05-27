0d107e8e49b77d490035873e0eae62a698bf1a50
MysteriousAcadia <anshumansingh64@gmail.com>
2021-05-27 10:36:33 +0530

    Initial Commit

---
+++ Assets/Scripts/ImageLoader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using Firebase;
+using Firebase.Extensions;
+using Firebase.Storage;
+
+public class ImageLoader : MonoBehaviour
+{
+    RawImage rawImage;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
+    IEnumerator LoadImage(string MediaUrl){
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl); //Create a request
+        yield return request.SendWebRequest(); //Wait for the request to complete
+        if(request.isNetworkError || request. isHttpError){
+            Debug.Log(request.error);
+        }
+        else{
+            rawImage.texture = ((DownloadHandlerTexture)request.downloadHandler).texture; 
+            // setting the loaded image to our object
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rawImage = gameObject.GetComponent<RawImage>();
+        // StartCoroutine(LoadImage("https://firebasestorage.googleapis.com/v0/b/vikings-login.appspot.com/o/VikingsLogo.jpeg?alt=media&token=44b39032-34fe-489e-bd69-536f0ed7c251"));
+        //Easy hardcoded solution But bad approach
+        
+        //initialize storage reference
+        storage = FirebaseStorage.DefaultInstance;
+        storageReference = storage.GetReferenceFromUrl("gs://vikings-login.appspot.com/");
+
+        //get reference of image
+        StorageReference image = storageReference.Child("VikingsLogo.jpeg");
+
+        //Get the download link of file
+        image.GetDownloadUrlAsync().ContinueWithOnMainThread(task =>
+        {
+            if(!task.IsFaulted && !task.IsCanceled){
+                StartCoroutine(LoadImage(Convert.ToString(task.Result))); //Fetch file from the link
+            }
+            else{
+                Debug.Log(task.Exception);
+            }
+        });
+    }
+
+}
---
+++ Assets/Scripts/UploadFile.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//For Picking files
+using System.IO;
+using SimpleFileBrowser;
+
+//For firebase storage
+using Firebase;
+using Firebase.Extensions;
+using Firebase.Storage;
+public class UploadFile : MonoBehaviour
+{
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        FileBrowser.SetFilters(true, new FileBrowser.Filter("Images", ".jpg", ".png"), new FileBrowser.Filter("Text Files", ".txt", ".pdf"));
+
+        FileBrowser.SetDefaultFilter(".jpg");
+
+        
+        FileBrowser.SetExcludedExtensions(".lnk", ".tmp", ".zip", ".rar", ".exe");
+        storage = FirebaseStorage.DefaultInstance;
+        storageReference = storage.GetReferenceFromUrl("gs://vikings-login.appspot.com/");
+
+
+    }
+
+    public void OnButtonClick(){
+        StartCoroutine(ShowLoadDialogCoroutine());
+
+    }
+
+    IEnumerator ShowLoadDialogCoroutine()
+    {
+       
+        yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.FilesAndFolders, true, null, null, "Load Files and Folders", "Load");
+
+        Debug.Log(FileBrowser.Success);
+
+        if (FileBrowser.Success)
+        {
+            // Print paths of the selected files (FileBrowser.Result) (null, if FileBrowser.Success is false)
+            for (int i = 0; i < FileBrowser.Result.Length; i++)
+                Debug.Log(FileBrowser.Result[i]);
+
+            Debug.Log("File Selected");
+            byte[] bytes = FileBrowserHelpers.ReadBytesFromFile(FileBrowser.Result[0]);
+            //Editing Metadata
+            var newMetadata = new MetadataChange();
+            newMetadata.ContentType = "image/jpeg";
+
+            //Create a reference to where the file needs to be uploaded
+            StorageReference uploadRef = storageReference.Child("uploads/newFile.jpeg");
+            Debug.Log("File upload started");
+            uploadRef.PutBytesAsync(bytes, newMetadata).ContinueWithOnMainThread((task) => { 
+                if(task.IsFaulted || task.IsCanceled){
+                    Debug.Log(task.Exception.ToString());
+                }
+                else{
+                    Debug.Log("File Uploaded Successfully!");
+                }
+            });
+
+
+        }
+    }
+   
+}
